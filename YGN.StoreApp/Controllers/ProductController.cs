46acf46d23fd881806e1870860612eb84ef031a0
Hüseyin <Hseyin@DESKTOP-QC9EOLC>
2024-03-06 16:15:33 +0300

    Add project files.

---
+++ YGN.StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YGN.StoreApp.Entities.Models;
+using YGN.StoreApp.Repositories;
+
+
+namespace YGN.StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _context; //DI
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model= _context.Products.ToList();
+            return View(model);
+        }
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(p=>p.ProductId.Equals(id));
+            return View(product);
+        }
+    }
+}
---
+++ YGN.StoreApp/Program.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.EntityFrameworkCore;
+using YGN.StoreApp.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
+
+        b => b.MigrationsAssembly("YGN.StoreApp"));
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); //wwwroot
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapGet("/btk", () => "BTK Akademi");
+app.Run();
