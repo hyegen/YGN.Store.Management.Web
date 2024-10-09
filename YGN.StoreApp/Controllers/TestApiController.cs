06cf2c51d23664cd5c5b2fd28d719e37b2c5de72
Hüseyin <hyegen205@gmail.com>
2024-10-09 21:53:12 +0300

    ####

---
+++ YGN.StoreApp/Controllers/TestApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YGN.Services.Contracts.Manager;
+
+namespace YGN.StoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public TestApiController(IServiceManager repositoryManager)
+        {
+            _serviceManager = repositoryManager;
+        }
+
+        [HttpGet]
+        public string GetProducts()
+        {
+            var result = _serviceManager.ProductService.GetAllProducts(false);
+
+            var json = JsonConvert.SerializeObject(result);
+            return json;
+        }
+    }
+}
